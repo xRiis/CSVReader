5385015ebd3421d526ae6898f70312e1078130d0
Riis Card <42218146+xRiis@users.noreply.github.com>
2021-03-02 17:09:23 -0700

    Added summary sheets, improved readability

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import glob, os
 import numpy as np
 import pandas as pd
 
-find_average = True
-find_sum = True
-find_region_area = True
-find_area_proportion = True
-skipped_rows = 11
+
+global value_count
+
+find_area_proportion, find_average, find_region_area, find_sum, write_summary = (True,) * 5
+
 column = 7
-find_val_offset = 4
+skipped_rows = 11
+
 find_val_column = 1
-fileDict = {}
+find_val_offset = 4
+
 dataDict = {}
-fileList = []
+fileDict = {}
+valueDict = {}
+
+averageList, fileList, proportionList, regionAreaList, sumList = ([] for i in range(5))
 
 
 def appendBlank(list, title, val):
@@ -57,39 +62,56 @@ def generateData(x, length, path):
     newList = []
     newLength = length
     if find_average == True:
-        newList = appendBlank(newList, "Avg. Area", calcAverage(x))
+        avg = calcAverage(x)
+        newList = appendBlank(newList, "Avg. Area", avg)
+        averageList.append(avg)
         newLength -= 3
     if find_sum == True:
-        newList = appendBlank(newList, "Sum Area", calcSum(x))
+        sum = calcSum(x)
+        newList = appendBlank(newList, "Sum Area", sum)
         newLength -= 3
+        sumList.append(sum)
     if find_region_area == True:
-        regionArea = findVal(path, (length + find_val_offset), find_val_column, x)
-        newList = appendBlank(newList, "Reg. Area", regionArea)
+        region_area = findVal(path, (length + find_val_offset), find_val_column, x)
+        regionArea = float(region_area)
+        newList = appendBlank(newList, "Reg. Area", region_area)
         newLength -= 3
+        regionAreaList.append(region_area)
         if find_area_proportion == True:
             sum = calcSum(x)
-            regionArea = float(regionArea)
             quot = sum / regionArea
             newList = appendBlank(newList, "Area Prop.", quot)
             newLength -= 3
+            proportionList.append(quot)
     for i in range(newLength):
         newList = np.append(newList, "")
+    dataDict["Avg. Area"] = averageList
+    dataDict["Sum Area"] = sumList
+    dataDict["Reg. Area"] = regionAreaList
+    dataDict["Area Prop."] = proportionList
     return newList
 
 
+def writeSummary(engine):
+    df = pd.DataFrame(dataDict)
+    df.to_excel(engine, sheet_name="Summary")
+
+
 def writeToFile(path):
-    writer = pd.ExcelWriter('file.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter("file.xlsx", engine="xlsxwriter")
     for i in range(len(fileList)):
         i_len = len(fileDict[fileList[i]])
         total_data = generateData(i, i_len, path)
+        if write_summary == True:
+            writeSummary(writer)
         df = pd.DataFrame({"Area": fileDict[fileList[i]],
                            "Data": total_data})
         df.to_excel(writer, sheet_name=fileList[i])
-        dataDict[fileList[i]] = df
     writer.save()
 
 
 if __name__ == '__main__':
+    value_count = 0
     directory = str(input("Please enter directory containing all CSV files to be read: "))
     findFiles(directory)
     writeToFile(directory)
